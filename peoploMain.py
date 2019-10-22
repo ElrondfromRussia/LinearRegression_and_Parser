bfb1cf15183e3234ec78b54bdd91b1b1ca5a4a04
m.gavrilenko <mag.shyz@yandex.ru>
2019-10-22 14:46:24 +0300

    changed html classes names
    Signed-off-by: m.gavrilenko <mag.shyz@yandex.ru>

--- peoploMain.py
+++ peoploMain.py
@@ -1,36 +1,39 @@
 import requests
 from bs4 import BeautifulSoup
 import csv
+import time
 
 def get_htm(url):
     R = requests.get(url)
     return R.text
 
 def get_pages(html):
+    #ЛУЧШЕ ПРОВЕРЯТЬ ЗНАЧЕНИЯ КЛАССОВЫХ ЛИТЕРАЛОВ
+    time.sleep(1)
     pages = []
     soup = BeautifulSoup(html, "html.parser")
-    pages_name = soup.find_all('a', class_="icMQ_ f-test-link- _3dPok")
+    pages_name = soup.find_all('a', class_="icMQ_ YYC5F f-test-link- _3dPok")
     obraovans = []
     experienses = []
     ages = []
     compensations = []
+    #print(pages_name)
     ############################################
     try:
         for i in range(len(pages_name)):
             ########зарплата##########
-            compensations.append("".join(pages_name[i].find('span', class_="_1h3Zg _3Q_Pz ADNB4 _1BoTZ _3ZLBy")
+            compensations.append("".join(pages_name[i].find('span', class_="_3mfro _3Q_Pz _1ZlLP _2JVkc")
                                      .find("span").find_all("span")[0].text.split()))
             ##########################
             ########возраст###########
-            if len(pages_name[i].find('span', class_="_1h3Zg z15vz _1xK5K _3ZLBy").find("span").find_all("span")) > 0:
-                ages.append(pages_name[i].find('span', class_="_1h3Zg z15vz _1xK5K _3ZLBy").find("span")
+            if len(pages_name[i].find('span', class_="_3mfro _9fXTd _2JVkc _3e53o").find("span").find_all("span")) > 0:
+                ages.append(pages_name[i].find('span', class_="_3mfro _9fXTd _2JVkc _3e53o").find("span")
                         .find_all("span")[0].text.split(" ")[0])
             else:
                 ages.append("30")
             ##########################
             #########опыт работы#######
-            opt_buf = pages_name[i].find('span', class_="_1h3Zg z15vz _1xK5K _3ZLBy").find("span")\
-                .find('span', class_= "_3AnJT")
+            opt_buf = pages_name[i].find('span', class_="_3mfro _9fXTd _2JVkc _3Ll36").find("span")
             int_buf = 0
             if opt_buf:
                 if len(opt_buf) > 0:
@@ -54,9 +57,9 @@ def get_pages(html):
                 experienses.append("0")
             #########################
             #########образование######
-            url1 = "https://www.superjob.ru" +  pages_name[i].get("href")
+            url1 = "https://www.superjob.ru" + pages_name[i].get("href")
             soup1 = BeautifulSoup(get_htm(url1), "html.parser")
-            obrs = soup1.find_all('div', class_="_1h3Zg z15vz _1xK5K _21a7u _2SvHc")
+            obrs = soup1.find_all('div', class_="_3mfro _9fXTd _2JVkc _3e53o _3LJqf _15msI")
             mstr = ''.join(map(str, obrs))
             obraa = 0
             if mstr.find("Среднее специальное образование") != -1:
@@ -66,6 +69,7 @@ def get_pages(html):
             obraovans.append(obraa)
             ##############################
     except:
+        print("error")
         pass
     ###########Итог########################
     for k in range(len(ages)):
@@ -104,7 +108,7 @@ writer = csv.writer(f1)
 #writer = csv.writer(f2)
 #writer = csv.writer(f3)
 #writer = csv.writer(f4)
-writer.writerow(("#","#age", "#opit", "#obrazovan", "#zp"))
+writer.writerow(("#age", "#opit", "#obrazovan", "#zp"))
 
 f1.close()
 #f2.close()
@@ -112,7 +116,7 @@ f1.close()
 #f4.close()
 
 
-for i in range(1, 51):
+for i in range(1, 100):
     print(i)
     url1 = "https://www.superjob.ru/resume/search_resume.html?payment_no_agreement=1&catalogues%5B0%5D=33&t%5B0%5D=4&page="+str(i)
     #url2 = "https://www.superjob.ru/resume/search_resume.html?payment_no_agreement=1&catalogues%5B0%5D=86&t%5B0%5D=4&page="+str(i)
@@ -120,7 +124,7 @@ for i in range(1, 51):
     #url4 = "https://www.superjob.ru/resume/search_resume.html?payment_no_agreement=1&catalogues%5B0%5D=197&t%5B0%5D=4&page="+str(i)
 
     pages1 = get_pages(get_htm(url1))
-    print(pages1)
+    #print(pages1)
     # pages2 = get_pages(get_htm(url2))
     #pages3 = get_pages(get_htm(url3))
     #pages4 = get_pages(get_htm(url4))
